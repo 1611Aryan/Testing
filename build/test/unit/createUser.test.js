2791c919da317393282232ef7252c753a226870d
Aryan <aryan2563737@gmail.com>
2023-01-17 17:04:17 +0530

    Stubs and proxy added

--- build/test/unit/createUser.test.js
+++ build/test/unit/createUser.test.js
@@ -1,10 +1,18 @@
 import { should } from "chai";
 import { describe } from "mocha";
-import getUser from "../../User/createUser.js";
+import proxyquire from "proxyquire";
+import Sinon from "sinon";
+import data from "./mock.js";
 describe("Actual User Test", function () {
     should();
+    const fetchStub = Sinon.stub();
+    fetchStub.resolves(data);
+    const getUserProxy = proxyquire("/../../User/createUser.js", {
+        "node-fetch": fetchStub,
+    });
     it("results recieved", async function () {
-        const res = await getUser();
+        const res = await getUserProxy();
+        console.log(res);
         res.should.have.property("results");
         res.results[0].should.have.property("name");
     });
---
+++ build/test/unit/mock.js
@@ -0,0 +1,48 @@
+const data = {
+    results: [
+        {
+            gender: "female",
+            name: { title: "Miss", first: "Johanne", last: "Madsen" },
+            location: {
+                street: { number: 6270, name: "Solparken" },
+                city: "Aarhus",
+                state: "Sjælland",
+                country: "Denmark",
+                postcode: 77606,
+                coordinates: { latitude: "-46.4691", longitude: "87.2490" },
+                timezone: {
+                    offset: "+6:00",
+                    description: "Almaty, Dhaka, Colombo",
+                },
+            },
+            email: "johanne.madsen@example.com",
+            login: {
+                uuid: "673cac0d-cbec-470d-9f4c-1ca411f96898",
+                username: "silverbird777",
+                password: "danger",
+                salt: "T5WCc229",
+                md5: "daf2723d2b3bb11803c3f2c661291b48",
+                sha1: "2471ad630a18499aa7b4658a2b865e8af638e6f0",
+                sha256: "53ca8d61680111a7f34c6e58c4e7d62dd4525f138725736ccbc25b6340a0a14c",
+            },
+            dob: { date: "1982-12-14T20:26:53.441Z", age: 40 },
+            registered: { date: "2007-06-26T09:51:59.187Z", age: 15 },
+            phone: "19535551",
+            cell: "07331149",
+            id: { name: "CPR", value: "141282-5676" },
+            picture: {
+                large: "https://randomuser.me/api/portraits/women/7.jpg",
+                medium: "https://randomuser.me/api/portraits/med/women/7.jpg",
+                thumbnail: "https://randomuser.me/api/portraits/thumb/women/7.jpg",
+            },
+            nat: "DK",
+        },
+    ],
+    info: {
+        seed: "69783524708610ec",
+        results: 1,
+        page: 1,
+        version: "1.4",
+    },
+};
+export default data;
